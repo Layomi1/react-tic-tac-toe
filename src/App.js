a454ee2f0dfdb3d238b263a8be294c9f6d611d63
Layomi1 <107419093+Layomi1@users.noreply.github.com>
2024-05-29 11:29:02 +0100

    implementing tic-tac-toe game

--- src/App.js
+++ src/App.js
@@ -1,23 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import Square from "./components/Square/Square";
 
+// defining patterns
+// 0 1 2
+// 3 4 5
+// 6 7 8
 function App() {
+  const [squares, setSquares] = useState(Array(9).fill(""));
+  const [isXTurn, setIsXTurn] = useState(true);
+  const [status, setStatus] = useState("");
+
+  function getWinner(squares) {
+    const winningPatterns = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+      [0, 3, 6],
+      [2, 5, 8],
+      [1, 4, 7],
+    ];
+
+    for (let i = 0; i < winningPatterns.length; i++) {
+      const [a, b, c] = winningPatterns[i];
+
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
+
+  useEffect(() => {
+    if (!getWinner(squares) && squares.every((item) => item !== "")) {
+      setStatus("This is a draw! Please, restart the game");
+    } else if (getWinner(squares)) {
+      setStatus(`Winner is ${getWinner(squares)}. Please, restart the ame`);
+    } else {
+      setStatus(`Next player is ${isXTurn ? "X" : "O"}`);
+    }
+  }, [squares, isXTurn]);
+
+  function handleClick(getCurrentSquare) {
+    let copySquares = [...squares];
+    if (getWinner(copySquares) || copySquares[getCurrentSquare]) return;
+    copySquares[getCurrentSquare] = isXTurn ? "X" : "O";
+    setIsXTurn(!isXTurn);
+    setSquares(copySquares);
+  }
+
+  function handleRestart() {
+    setIsXTurn(true);
+    setSquares(Array(9).fill(""));
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="squares">
+        <div className="row">
+          <Square value={squares[0]} onClick={() => handleClick(0)} />
+          <Square value={squares[1]} onClick={() => handleClick(1)} />
+          <Square value={squares[2]} onClick={() => handleClick(2)} />
+        </div>
+        <div className="row">
+          <Square value={squares[3]} onClick={() => handleClick(3)} />
+          <Square value={squares[4]} onClick={() => handleClick(4)} />
+          <Square value={squares[5]} onClick={() => handleClick(5)} />
+        </div>
+        <div className="row">
+          <Square value={squares[6]} onClick={() => handleClick(6)} />
+          <Square value={squares[7]} onClick={() => handleClick(7)} />
+          <Square value={squares[8]} onClick={() => handleClick(8)} />
+        </div>
+      </div>
+
+      <h1>{status}</h1>
+      <button className="btn" onClick={handleRestart}>
+        Restart
+      </button>
     </div>
   );
 }
---
+++ src/components/Square/Square.jsx
@@ -0,0 +1,11 @@
+import styles from "./Square.module.css";
+
+const Square = ({ value, onClick }) => {
+  return (
+    <button className={styles.square} onClick={onClick}>
+      {value}
+    </button>
+  );
+};
+
+export default Square;
